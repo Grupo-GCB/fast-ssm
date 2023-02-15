1e86c59270e0da33022164349ba4da05ce5a97cb
Iago Sousa <sousair2001@gmail.com>
2023-02-15 20:35:20 -0300

    refactor: remove redis use cache in memory

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { spawnSync } from 'child_process';
 import { AWSParameterStore } from './aws-parameter-store';
-import { Redis, rl } from './redis';
 import { getSsmSync } from './ssm';
 import { Cache, GetParams, LogLevel } from './types';
+import { Cache as CacheClass } from './cache';
 import { getMiliseconds } from './utils';
 
 let logLevel: LogLevel = 'error';
@@ -10,41 +9,35 @@ let throwError = false;
 let defaultCacheTime = 30;
 let region = 'us-east-1';
 const cache: Cache = {};
+const FIVE_HOURS_IN_MS = 5 * 60 * 60 * 1000;
 
 export namespace IGetSync {
   export type Params = {
     path: string;
     region?: string;
+    cacheTime?: number;
   };
   export type Result = string;
 }
 
-export function getParameterSync({ path, region = 'us-east-1' }: IGetSync.Params): IGetSync.Result {
-  Redis.setupRedisClient();
-  const bufferFromCache = spawnSync('node', [__dirname, './redis'], {
-    input: path,
-    maxBuffer: 4000000,
-  });
+export function getParameterSync({
+  path,
+  region = 'us-east-1',
+  cacheTime = FIVE_HOURS_IN_MS,
+}: IGetSync.Params): IGetSync.Result {
+  const valueFromCache = CacheClass.get(path);
 
-  rl.close();
-
-  const valueFromCache = bufferFromCache.output.toString().split(',')[1].replace('\n', '');
-
-  if (valueFromCache !== 'null') {
-    Redis.disconnectRedisClient();
+  if (valueFromCache) {
     return valueFromCache;
   }
-  
+
   const valueFromSSM = AWSParameterStore.getParameter(path, region);
-  
+
   if (valueFromSSM) {
-    Redis.save(path, valueFromSSM);
-    Redis.disconnectRedisClient();
-    rl.close();
+    CacheClass.set(path, valueFromSSM, cacheTime);
     return valueFromSSM;
   }
-  
-  Redis.disconnectRedisClient();
+
   return '';
 }
 
